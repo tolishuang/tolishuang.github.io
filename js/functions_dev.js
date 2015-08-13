86b120a8b1b6c7988a07243b32a9d4b33ec936c8
tolishuang <hfsun301@gmail.com>
2015-08-12 21:51:28 -0400

    third

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -94,7 +94,7 @@ function startHeartAnimation() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = - (Date.parse(current) - Date.parse(date)) / 1000;
+	var seconds = (Date.parse(date) - Date.parse(current)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
